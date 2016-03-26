489f356bba7f2fd98c072dedc10ce7e2b64935d6
DrMarkes <markovandrew@yandex.ru>
2016-03-27 00:11:15 +0700

    Configure base Project and create Reminder controller

---
+++ src/main/java/ru/drmarkes/remindme/server/config/WebConfig.java
@@ -0,0 +1,11 @@
+package ru.drmarkes.remindme.server.config;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+
+@Configuration
+@EnableWebMvc
+@ComponentScan("ru.drmarkes.remindme.server")
+public class WebConfig {
+}
