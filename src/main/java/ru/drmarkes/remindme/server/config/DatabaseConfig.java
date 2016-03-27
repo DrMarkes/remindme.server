d3c057e2d3c51153b9d86d2842b85ed1535d6ac5
DrMarkes <markovandrew@yandex.ru>
2016-03-27 19:54:55 +0700

    Configure database

---
+++ src/main/java/ru/drmarkes/remindme/server/config/DatabaseConfig.java
@@ -0,0 +1,21 @@
+package ru.drmarkes.remindme.server.config;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.core.env.Environment;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
+
+import javax.annotation.Resource;
+
+@Configuration
+@EnableJpaRepositories("ru.drmarkes.remindme.server.repository")
+@EnableTransactionManagement
+@PropertySource("classpath:db.properties")
+@ComponentScan("ru.drmarkes.remindme.server")
+public class DatabaseConfig {
+
+    @Resource
+    private Environment env;
+}
